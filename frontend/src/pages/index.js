6c915cc5939c6803730faebcd56120417d663cbe
abitechcoder <abiolaolalekan39@gmail.com>
2023-08-25 07:39:12 +0100

    Setting up of react router client side routing for pages

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,6 @@
+import RootLayout from "./RootLayout";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import LandingPage from "./LandingPage";
+
+export {RootLayout, Login, SignUp, LandingPage}
