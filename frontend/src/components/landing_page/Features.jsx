0e82caf146b4088a109453e874b1b06cf47b527f
abitechcoder <abiolaolalekan39@gmail.com>
2023-08-28 14:13:31 +0100

    Worked on the Hero, Downloads, Contact Us section of the landing page

--- frontend/src/components/landing_page/Features.jsx
+++ frontend/src/components/landing_page/Features.jsx
@@ -5,16 +5,19 @@ import { FeatureList } from "./FeaturesSect";
 function Features() {
   const features = [
     {
+      id: 1,
       image: featureIcon1,
       title: "Share notes with friends",
       text: "Whether you have a team of 2 or 200, our shared team inboxes keep everyone on the same page and in the loop.",
     },
     {
+      id: 2,
       image: featureIcon2,
       title: "Auto Completion",
       text: "An all-in-one customer service platform that helps you balance everything your customers need to be happy.",
     },
     {
+      id: 3,
       image: featureIcon3,
       title: "Manage your notes with friends",
       text: "Measure what matters with Untitled’s easy-to-use reports. You can filter, export, and drilldown on the data in a couple clicks.",
@@ -22,8 +25,12 @@ function Features() {
   ];
   return (
     <div>
-      <h2 className="text-[57px] font-medium mb-9 text-center">Features</h2>
-      <FeatureList features={features} />
+      <div className="custom-container">
+        <h2 className="text-[40px] md:text-[57px] font-medium mb-9 text-center font-clash">
+          Features
+        </h2>
+        <FeatureList features={features} />
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/landing_page/Testimonials.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Logomark, Derick } from "../../assets";
+
+function Testimonials() {
+  const dots = [1, 2, 3, 4, 5];
+  return (
+    <div className="bg-black text-white">
+      <div className="custom-container ">
+        <div className="grid gap-6 lg:gap-12 place-items-center">
+
+        <img src={Logomark} alt="" />
+        <p className="font-dm font-medium text-center text-lg lg:text-2xl md:w-[70%] lg:w-[50%]">
+          I use Note.d for all my note taking when working on a project. I send
+          it over to clients to crosscheck for me.
+        </p>
+        <div className="grid place-items-center gap-3">
+          <img src={Derick} className="w-[50px]" alt="Image of Dericks" />
+          <div>
+            <h4 className="text-center font-bold text-base font-inter">Derick Tsorme</h4>
+            <p className="text-center text-xs font-inter">Product Designer</p>
+          </div>
+        </div>
+        <div className="flex gap-1">
+          {dots.map((dot) => {
+            if (dot === 3) {
+              return (
+                <div className="w-2 h-2 bg-white rounded-full" key={dot}></div>
+              );
+            } else {
+              return (
+                <div
+                  className="w-2 h-2 bg-[#A1A1A1] rounded-full"
+                  key={dot}
+                ></div>
+              );
+            }
+          })}
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonials;
