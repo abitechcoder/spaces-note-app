cfccd508c80ec4bf66f000e5954024261c103fa8
agyanim <agyanimconsultancy@gmail.com>
2023-08-31 17:00:11 +0000

    server side google apis installed to get access token

--- backend/util/sendMail.js
+++ backend/util/sendMail.js
@@ -1,6 +1,8 @@
 import nodemailer from "nodemailer"
 import dotenv from "dotenv"
+import {google} from "googleapis"
 dotenv.config()
+
 
 export const sendEmail=async(userEmail)=>{
     
@@ -8,8 +10,14 @@ export const sendEmail=async(userEmail)=>{
     const PASS=process.env.MAIL_PASSWORD
     const CLIENTID=process.env.OAUTH_CLIENTID
     const CLIENTSECRET=process.env.OAUTH_CLIENT_SECRET
-    const ACCESSTOKEN=process.env.ACCESS_TOKEN
+    // const ACCESSTOKEN=process.env.ACCESS_TOKEN
     const REFRESHTOKEN=process.env.OAUTH_REFRESH_TOKEN
+    const REDIRECTURL=process.env.REDIRECT_URL
+try {
+    // setting up google oauth2 credentiala to get an access token
+    const oAuth2Client= new google.auth.OAuth2(CLIENTID,CLIENTSECRET,REDIRECTURL)
+    // getting the access token function
+    const accessToken= oAuth2Client.setCredentials({refresh_token:REFRESHTOKEN})
     const transporter=nodemailer.createTransport({
         service:"gmail",
         auth:{
@@ -18,14 +26,14 @@ export const sendEmail=async(userEmail)=>{
             pass:PASS,
             clientId:CLIENTID,
             clientSecret:CLIENTSECRET,
-            accessToken:ACCESSTOKEN,
+            accessToken:accessToken,
             refreshToken:REFRESHTOKEN
     
         }
     })
     const mailMessage={
         from:"agyanimitsolusions@gmail.com",
-        to:`${userEmail}`,
+        to:`spacenoteapp7@gmail.com`,
         subject:"space-note-app",
         text:`Hi, you have successfully created an account with us.`
     }
@@ -35,6 +43,10 @@ export const sendEmail=async(userEmail)=>{
         }
         console.log("email notification sent successfully");
     })
+
+} catch (error) {
+    console.log(error.message);
+}
 }
 
     
