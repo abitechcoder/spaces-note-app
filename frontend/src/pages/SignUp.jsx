2ebf56ef060e8321b112cb7886aff8741ba79121
Adeosun Abiola <abiolaolalekan39@gmail.com>
2023-09-05 16:51:16 +0100

    Worked on validation of signup inputs

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -4,17 +4,36 @@ import { AiOutlineQuestionCircle } from "react-icons/ai";
 import { TextInput } from "../components/auth";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import { Navbar } from "../components/landing_page";
+import { useForm } from "react-hook-form";
 
 function SignUp() {
   const navigate = useNavigate();
   const location = useLocation();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  console.log("Error:", errors);
+
+  const onSubmit = (data) => console.log(data);
   return (
     <section>
       <div className="p-4 lg:p-0">
         <Navbar currentLocation={location.pathname} />
       </div>
       <div className="min-h-[90vh] bg-white grid place-items-center text-black">
-        <div className="w-full md:w-[70%] lg:w-[50%] px-4">
+        <form
+          className="w-full md:w-[70%] lg:w-[50%] px-4"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <h1 className="font-inter text-2xl md:text-4xl pb-4 md:pb-8 text-center md:text-left">
             Sign-up
           </h1>
@@ -29,17 +48,48 @@ function SignUp() {
             </button>
           </div>
           <div className="py-8 grid gap-4">
-            <TextInput type="email" placeholder="E-MAIL" />
+            <TextInput
+              type="email"
+              placeholder="E-MAIL"
+              {...register("email", {
+                pattern: {
+                  value: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
+                  message: "Bad Email Address Format",
+                },
+                required: "Email Address is required",
+              })}
+              aria-invalid={errors.email ? "true" : "false"}
+            />
+            {errors.email && <p role="alert">{errors.email.message}</p>}
             <div className="flex gap-2 items-center">
-              <TextInput type="password" placeholder="PASSWORD" />
+              <TextInput
+                type="password"
+                placeholder="PASSWORD"
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 8,
+                    message: "Password must be more than 8 characters",
+                  },
+                  maxLength: {
+                    value: 30,
+                    message: "Password must be less than 30 characters",
+                  },
+                })}
+                aria-invalid={errors.password ? "true" : "false"}
+              />
               <AiOutlineQuestionCircle size={30} color="#242424" />
             </div>
+            {errors.password && <p role="alert">{errors.password.message}</p>}
             <p className="text-gray-500 font-dm">FORGOT PASSWORD?</p>
           </div>
 
           <div className="grid place-items-center">
             <div className="flex items-center gap-4">
-              <button className="custom-button font-dm bg-[#7F6BFF] text-white">
+              <button
+                className="custom-button font-dm bg-[#7F6BFF] text-white"
+                type="submit"
+              >
                 SIGN UP
               </button>
               <button
@@ -50,7 +100,7 @@ function SignUp() {
               </button>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </section>
   );
