217cbfeed207b5aacb5214f133d187a957967786
abitechcoder <abiolaolalekan39@gmail.com>
2023-09-08 16:31:25 +0100

    made the side navbar scrollable

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,25 +5,25 @@ import { CgFileDocument } from "react-icons/cg";
 import { FoldersComponent } from "../components/folder_category";
 import MoreSectionComponent from "../components/more_section/MoreSectionComponent";
 import { useSelector, useDispatch } from "react-redux";
-import {logout, reset} from '../features/auth/authSlice'
+import { logout, reset } from "../features/auth/authSlice";
 import { useNavigate } from "react-router";
 
 function Dashboard() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const {user} = useSelector((state) => state.auth)
+  const { user } = useSelector((state) => state.auth);
 
   const onLogout = () => {
-    dispatch(logout())
-    dispatch(reset())
-    navigate('/login')
-  }
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/login");
+  };
 
   return (
     <section className="h-screen grid grid-cols-[300px_350px_1fr] bg-[#181818]">
-      <div className="py-[30px]">
-        <div className="px-[20px]">
+      <div className="py-[30px] overflow-y-scroll">
+        <div className="w-[280px] px-[20px] bg-[#181818]">
           <div className="flex items-center justify-between">
             <img src={LogoWhite} alt="" className="h-[50px]" />
             <CiSearch size={25} color="#ffffff" />
@@ -34,46 +34,49 @@ function Dashboard() {
           </button>
         </div>
 
-				<div className="pt-[30px] grid gap-4">
-					<h5 className="px-[20px] text-[14px] font-semibold font-sans text-white text-opacity-60">
-						Recents
-					</h5>
-					<div className="grid gap-2">
-						<div className="px-[20px] py-[10px] flex gap-2 bg-[#312EB5]">
-							<CgFileDocument size={20} color="#ffffff" opacity={1} />
-							<p className="text-white">Reflection on the Month of June</p>
-						</div>
-						<div className="px-[20px] py-[10px] flex gap-2">
-							<CgFileDocument size={20} color="#ffffff" opacity={0.6} />
-							<p className="text-white text-opacity-60">
-								Reflection on the Month of June
-							</p>
-						</div>
-						<div className="px-[20px] py-[10px] flex gap-2">
-							<CgFileDocument size={20} color="#ffffff" opacity={0.6} />
-							<p className="text-white text-opacity-60">
-								Reflection on the Month of June
-							</p>
-						</div>
-					</div>
-				</div>
-				{/* Folder category section starts here*/}
-				<div className="mt-5">
-					<FoldersComponent />
-					<MoreSectionComponent />
-				</div>
-				{/* Folder category section ends here*/}
+        <div className="pt-[30px] grid gap-4">
+          <h5 className="px-[20px] text-[14px] font-semibold font-sans text-white text-opacity-60">
+            Recents
+          </h5>
+          <div className="grid gap-2">
+            <div className="px-[20px] py-[10px] flex gap-2 bg-[#312EB5]">
+              <CgFileDocument size={20} color="#ffffff" opacity={1} />
+              <p className="text-white">Reflection on the Month of June</p>
+            </div>
+            <div className="px-[20px] py-[10px] flex gap-2">
+              <CgFileDocument size={20} color="#ffffff" opacity={0.6} />
+              <p className="text-white text-opacity-60">
+                Reflection on the Month of June
+              </p>
+            </div>
+            <div className="px-[20px] py-[10px] flex gap-2">
+              <CgFileDocument size={20} color="#ffffff" opacity={0.6} />
+              <p className="text-white text-opacity-60">
+                Reflection on the Month of June
+              </p>
+            </div>
+          </div>
+        </div>
+        {/* Folder category section starts here*/}
+        <div className="mt-5">
+          <FoldersComponent />
+          <MoreSectionComponent />
+        </div>
+        {/* Folder category section ends here*/}
 
         <div>
-        <button onClick={() => onLogout()} className="py-[15px] bg-red-700 text-white w-[80%] mx-auto rounded-lg">Logout</button>
+          <button
+            onClick={() => onLogout()}
+            className="py-[15px] bg-red-700 text-white w-[80%] mx-auto rounded-lg"
+          >
+            Logout
+          </button>
         </div>
-			</div>
-			<div className="bg-[#1c1c1c]"></div>
-			<div>
-        
       </div>
-		</section>
-	);
+      <div className="bg-[#1c1c1c]"></div>
+      <div></div>
+    </section>
+  );
 }
 
 export default Dashboard;
