781358e1b3cf4abefad9159ebca6bbcd33c8b621
abitechcoder <abiolaolalekan39@gmail.com>
2023-10-03 19:46:54 +0100

    Fixed merge conflicts on the frontend

--- frontend/src/components/auth/TextInput.jsx
+++ frontend/src/components/auth/TextInput.jsx
@@ -1,17 +1,5 @@
 import React from 'react'
 
-<<<<<<< HEAD
-function TextInput({...props}) {
-  return (
-    <div>
-      <input
-      {...props}
-      className="w-full rounded md:rounded-lg py-2 px-4 border-[1.5px] bg-[#f7f7f] border-[#898989] border-opacity-30"
-    />
-    </div>
-  )
-}
-=======
 const TextInput = React.forwardRef((props, ref) => (
   <input
     ref={ref}
@@ -19,7 +7,6 @@ const TextInput = React.forwardRef((props, ref) => (
     className={`w-full rounded md:rounded-lg py-2 px-4 border-[1.5px] bg-[#f7f7f] border-[#898989] border-opacity-30 ${props.className}`}
   />
 ));
->>>>>>> 1a62ccf9566d93fddc4574e90ae02c8781d8c378
 
 export default TextInput;
 
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,72 +1,28 @@
-<<<<<<< HEAD
-import React, { useContext } from "react";
-import LogoWhite from "../assets/LogoWhite.png";
-import { CiSearch } from "react-icons/ci";
-import { AiOutlinePlus } from "react-icons/ai";
-import {CgFileDocument} from "react-icons/cg"
-import Spaces from "../components/Dashboard/Spaces";
-import Reflection from "../components/Dashboard/Reflection"
-import { TextContext } from '../util/TextContext.jsx'
-
-
-
-
-function Dashboard() {
-  const { text, setText}  = useContext(TextContext)
-  
-
-const  textHandler = () => {
-  setText("true")
-}
-
-
-
-  return (
-    <section className="h-screen grid grid-cols-[300px_350px_1fr] bg-[#181818]">
-      <div className="py-[30px]">
-        <div className="px-[20px]">
-          <div className="flex items-center justify-between">
-            <img src={LogoWhite} alt="" className="h-[50px]" />
-            <CiSearch size={25} color="#ffffff" />
-          </div>
-          <button className="mt-[30px] w-full p-[10px] rounded-lg bg-[#ffffff] bg-opacity-5 text-white flex justify-center items-center gap-2">
-            <AiOutlinePlus className="inline-block" size={25} />
-            <p className="font-bold font-sans" onClick={textHandler}>New Note</p>
-          </button>
-        </div>
-=======
 import React, { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { useSelector, useDispatch } from "react-redux";
 
-import { logout, reset, setRefreshToken } from "../features/auth/authSlice";
+import { logout, reset } from "../features/auth/authSlice";
 
 import { LogoWhite } from "../assets";
 import { CiSearch } from "react-icons/ci";
 import { AiOutlinePlus } from "react-icons/ai";
-import { FoldersComponent } from "../components/folder_category";
 import MoreSectionComponent from "../components/more_section/MoreSectionComponent";
-import authService from "../features/auth/authService";
+
 import {
   RecentNotes,
   NewNoteDialog,
   NotesFolders,
-  // NotesList,
   Main,
 } from "../components/dashboard_page";
-// import { useFolderCategoryContext } from "../context/folderCategoryContex";
-// import NoteCategoryComponent from "../components/folder_category/NoteCategoryComponent";
-import { TextContext } from "../util/TextContext.jsx";
 import { DashboardContextProvider } from "../context/DashboardContextProvider";
 
 function Dashboard() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   let [isOpen, setIsOpen] = useState(false);
->>>>>>> 1a62ccf9566d93fddc4574e90ae02c8781d8c378
 
   const { user} = useSelector((state) => state.auth);
-  const email = user?.userAccount?.email;
   useEffect(() => {
     if (!user) {
       navigate("/login");
@@ -97,24 +53,6 @@ function Dashboard() {
                 <p className="font-bold font-sans">New Note</p>
               </button>
             </div>
-<<<<<<< HEAD
-            <div className="px-[20px] py-[10px] flex gap-2">
-              <CgFileDocument size={20} color="#ffffff" opacity={0.6}/>
-              <p className="text-white text-opacity-60">Project proposal</p>
-            </div>
-            <div className="px-[20px] py-[10px] flex gap-2">
-              <CgFileDocument size={20} color="#ffffff" opacity={0.6}/>
-              <p className="text-white text-opacity-60">Travel itinerary</p>
-            </div>
-          </div>
-        </div>
-        <div><Spaces/></div>
-      </div>
-      <div>Personal</div>
-      <div><Reflection/></div>
-    </section>
-  )
-=======
 
             {/* List of Recent Notes */}
             <RecentNotes />
@@ -141,7 +79,6 @@ function Dashboard() {
       </>
     </DashboardContextProvider>
   );
->>>>>>> 1a62ccf9566d93fddc4574e90ae02c8781d8c378
 }
 
 export default Dashboard;
