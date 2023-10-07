b90c80a7aa2d544bbb981863667ad24a51604b0b
Agyanim <71318355+Agyanim@users.noreply.github.com>
2023-10-07 20:10:45 +0000

    server side archived endpoint added

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 // using cors 
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"http://127.0.0.1:5173",//http://localhost:5173
   methods:"GET, POST, PUT, DELETE",
   credentials:true
 }))
