cc72c1e8dfc734be955d29d4a309349a9650227e
Agyanim <71318355+Agyanim@users.noreply.github.com>
2023-10-19 06:38:11 +0000

    server side view ubuntu font added

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './public/views/**/*.html',
-    // './components/**/*.html,js',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./public/views/**/*.html"],
+	theme: {
+		extend: {
+			fontFamily: {
+				alanta: ["Alata", "sans-serif"],
+				roboto: ["Roboto Slab", "serif"],
+				ubuntu: ["Ubuntu", " sans - serif"],
+			},
+		},
+	},
+	plugins: [],
+};
